f06af2baa309483da9720639021bc0e3d9765242
tim30531-sudo <tim30531@gmail.com>
2026-08-03 19:36:13 +0200

    Füge Mod-Hauptklasse hinzu

---
+++ src/main/java/de/tim30531/deathknightscreen/DeathKnightScreenMod.java
@@ -0,0 +1,8 @@
+package de.tim30531.deathknightscreen;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(DeathKnightScreenMod.MOD_ID)
+public final class DeathKnightScreenMod {
+    public static final String MOD_ID = "deathknightscreen";
+}
